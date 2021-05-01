869ed2bf69823f015a729666bac6316048c86643
DicksonC96 <66625723+Dickson96@users.noreply.github.com>
2021-05-01 23:30:56 +0800

    Improved some codes

--- downloader.py
+++ downloader.py
@@ -1,14 +1,15 @@
 import requests
-for i in range(868, 3000):
-    url = "https://abcd.voxzer.org/stream/5fe6f2323fac5933e1e4b85b/1080/index"+str(i)+".ts"
+
+url = "https://abcd.voxzer.org/stream/604391840b8bd18237c8f91f/1080/index"
+for i in range(1300):
     try:
-        r = requests.get(url, stream=True)
+        r = requests.get(url+str(i)+".ts", stream=True)
         with open(str(i)+".ts","wb") as f:
-            for chunk in r.iter_content(chunk_size=1024):
+            for chunk in r.iter_content(chunk_size=None):
+                print("Downloading "+str(i)+".ts ...")
                 if chunk:
                     f.write(chunk)
-                    print("Downloading "+str(i)+".ts ...")
         r.close()
     except:
-        print("Download stopped at "+str(i))
+        print(r.raise_for_status())
         break
