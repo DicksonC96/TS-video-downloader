651574d58f1338827b0869479fab179b0a6c55f3
DicksonC96 <66625723+Dickson96@users.noreply.github.com>
2021-05-30 18:41:46 +0800

    Updated printout message to give more information on downloading progress

--- tsdownloaderv4.py
+++ tsdownloaderv4.py
@@ -7,7 +7,7 @@ def main(url, fname, mini=0, maxi=5000):
         if not mini==0:
             for i in range(mini):
                 r = requests.get(url+str(i)+".ts", stream=True)
-                sys.stdout.write("Downloading "+str(i)+".ts ...\n")
+                sys.stdout.write("Downloading "+str(i)+".ts ... ("+str(round((i/maxi*100),4))+"% in <"+str(maxi)+" iterations)\n")
                 sys.stdout.flush()
                 for chunk in r.iter_content(chunk_size=None):
                     if chunk:
@@ -19,7 +19,7 @@ def main(url, fname, mini=0, maxi=5000):
                 print("Download finished with "+str(i+1)+" iterations.")
                 break
             else:
-                sys.stdout.write("Downloading "+str(i)+".ts ...\n")
+                sys.stdout.write("Downloading "+str(i)+".ts ... ("+str(round((i/maxi*100),4))+"% in <"+str(maxi)+" iterations)\n")
                 sys.stdout.flush()
                 for chunk in r.iter_content(chunk_size=None):
                     if chunk:
@@ -29,8 +29,8 @@ def main(url, fname, mini=0, maxi=5000):
 def mp4convert(ts, mp4):
     subprocess.run(['ffmpeg', '-i', ts, mp4])
 
-url = "https://abcd.voxzer.org/stream/5fa56f253fac5933e1e4b589/1080/index"
-filename = "The New Mutants"
+url = "https://abcd.voxzer.org/stream/6082ef4d0b8bd18237c8fc11/1080/index"
+filename = "Mortal Kombat"
 ### main(url, filename, minimum_iteration, maximum_iteration)
-main(url, filename, 1600, 1700)
+main(url, filename, 3200, 3300)
 mp4convert(filename+".ts", filename+".mp4")
